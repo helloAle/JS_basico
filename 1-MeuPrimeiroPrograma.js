8b890104851b75ffcedbe523c402e4916337d669
Alexandre Oliveira <89224363+helloAle@users.noreply.github.com>
2022-02-17 16:05:38 -0300

    Add files via upload

---
+++ 1-MeuPrimeiroPrograma.js
@@ -0,0 +1,11 @@
+console.log("meu primeiro programa: trabalhando com variáveis");
+
+const idade = 20;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadesomada = idade + 2;
+console.log(idadesomada);
